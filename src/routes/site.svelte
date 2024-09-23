d87a76a10906b68dcda694ab28536a4b445f11dd
Tecksy <josephben2005@outlook.com>
2024-09-23 14:53:42 +0100

    Commiting from Laptop

---
+++ src/routes/site.svelte
@@ -0,0 +1,11 @@
+<script>
+    import resolution from "./config.json";
+</script>
+<div class="site"></div> 
+<style >
+    .site {
+        width: 100px;
+        height: 100px;
+        background-color: aqua;
+    }
+</style>
